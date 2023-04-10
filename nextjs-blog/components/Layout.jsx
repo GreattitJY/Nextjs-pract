6918b78880ed6561499696821b9f82ff9e63edce
GreattitJY <great.tit.jaejae@gmail.com>
2023-04-10 23:13:04 +0900

    add: 커스텀 sticky 및 top, bottom scroll 버튼

--- nextjs-blog/components/Layout.jsx
+++ nextjs-blog/components/Layout.jsx
@@ -1,6 +1,11 @@
 import React from "react";
+import styled from "styled-components";
 
 export default function Layout({ children }) {
   console.log(children);
   return <div>{children}</div>;
 }
+
+const Box = styled.div`
+  /* padding: 10px 0; */
+`;
--- nextjs-blog/pages/posts/first-post.jsx
+++ nextjs-blog/pages/posts/first-post.jsx
@@ -4,18 +4,191 @@ import { profile } from "../../src/assets";
 import Head from "next/head";
 import Script from "next/script";
 import Layout from "../../components/Layout";
+import styled from "styled-components";
+import { useEffect, useState } from "react";
 
 export default function FirstPost() {
+  const [scrollFlag, setScrollFlag] = useState(false);
+
+  const updateScroll = () => {
+    const { scrollY } = window;
+    const isScrolled = scrollY !== 0;
+    setScrollFlag(isScrolled);
+  };
+
+  const throttle = (callback, delay) => {
+    let timer = null;
+    return () => {
+      if (timer) return;
+      timer = window.setTimeout(() => {
+        callback();
+        timer = null;
+      }, delay);
+    };
+  };
+
+  const handleScroll = throttle(updateScroll, 100);
+
+  const handleToBottom = () => {
+    window.scrollTo({
+      top: document.body.scrollHeight,
+      behavior: "smooth",
+    });
+  };
+  const handleToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <Layout>
       <Head>
         <title>First Post</title>
       </Head>
+      <button onClick={handleToBottom}>down</button>
       <h1>First Post</h1>
       <h2>
         <Link href="/">Back to home</Link>
       </h2>
-      <Image src={profile} height={144} width={144} alt="Your Name" />
+      {scrollFlag && <Box>Text check</Box>}
+      <List>
+        <li>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, doloremque? Molestias repudiandae totam deserunt
+          ipsam, a labore fuga provident tempora hic voluptate? Repellendus alias, minus modi rem nisi officiis nemo!
+        </li>
+        <li>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, doloremque? Molestias repudiandae totam deserunt
+          ipsam, a labore fuga provident tempora hic voluptate? Repellendus alias, minus modi rem nisi officiis nemo!
+        </li>
+        <li>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, doloremque? Molestias repudiandae totam deserunt
+          ipsam, a labore fuga provident tempora hic voluptate? Repellendus alias, minus modi rem nisi officiis nemo!
+        </li>
+        <li>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, doloremque? Molestias repudiandae totam deserunt
+          ipsam, a labore fuga provident tempora hic voluptate? Repellendus alias, minus modi rem nisi officiis nemo!
+        </li>
+        <li>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, doloremque? Molestias repudiandae totam deserunt
+          ipsam, a labore fuga provident tempora hic voluptate? Repellendus alias, minus modi rem nisi officiis nemo!
+        </li>
+        <li>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, doloremque? Molestias repudiandae totam deserunt
+          ipsam, a labore fuga provident tempora hic voluptate? Repellendus alias, minus modi rem nisi officiis nemo!
+        </li>
+        <li>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, doloremque? Molestias repudiandae totam deserunt
+          ipsam, a labore fuga provident tempora hic voluptate? Repellendus alias, minus modi rem nisi officiis nemo!
+        </li>
+        <li>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, doloremque? Molestias repudiandae totam deserunt
+          ipsam, a labore fuga provident tempora hic voluptate? Repellendus alias, minus modi rem nisi officiis nemo!
+        </li>
+        <li>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, doloremque? Molestias repudiandae totam deserunt
+          ipsam, a labore fuga provident tempora hic voluptate? Repellendus alias, minus modi rem nisi officiis nemo!
+        </li>
+        <li>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, doloremque? Molestias repudiandae totam deserunt
+          ipsam, a labore fuga provident tempora hic voluptate? Repellendus alias, minus modi rem nisi officiis nemo!
+        </li>
+        <li>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, doloremque? Molestias repudiandae totam deserunt
+          ipsam, a labore fuga provident tempora hic voluptate? Repellendus alias, minus modi rem nisi officiis nemo!
+        </li>
+        <li>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, doloremque? Molestias repudiandae totam deserunt
+          ipsam, a labore fuga provident tempora hic voluptate? Repellendus alias, minus modi rem nisi officiis nemo!
+        </li>
+        <li>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, doloremque? Molestias repudiandae totam deserunt
+          ipsam, a labore fuga provident tempora hic voluptate? Repellendus alias, minus modi rem nisi officiis nemo!
+        </li>
+      </List>
+      <List>
+        <li>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, doloremque? Molestias repudiandae totam deserunt
+          ipsam, a labore fuga provident tempora hic voluptate? Repellendus alias, minus modi rem nisi officiis nemo!
+        </li>
+        <li>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, doloremque? Molestias repudiandae totam deserunt
+          ipsam, a labore fuga provident tempora hic voluptate? Repellendus alias, minus modi rem nisi officiis nemo!
+        </li>
+        <li>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, doloremque? Molestias repudiandae totam deserunt
+          ipsam, a labore fuga provident tempora hic voluptate? Repellendus alias, minus modi rem nisi officiis nemo!
+        </li>
+        <li>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, doloremque? Molestias repudiandae totam deserunt
+          ipsam, a labore fuga provident tempora hic voluptate? Repellendus alias, minus modi rem nisi officiis nemo!
+        </li>
+        <li>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, doloremque? Molestias repudiandae totam deserunt
+          ipsam, a labore fuga provident tempora hic voluptate? Repellendus alias, minus modi rem nisi officiis nemo!
+        </li>
+        <li>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, doloremque? Molestias repudiandae totam deserunt
+          ipsam, a labore fuga provident tempora hic voluptate? Repellendus alias, minus modi rem nisi officiis nemo!
+        </li>
+        <li>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, doloremque? Molestias repudiandae totam deserunt
+          ipsam, a labore fuga provident tempora hic voluptate? Repellendus alias, minus modi rem nisi officiis nemo!
+        </li>
+        <li>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, doloremque? Molestias repudiandae totam deserunt
+          ipsam, a labore fuga provident tempora hic voluptate? Repellendus alias, minus modi rem nisi officiis nemo!
+        </li>
+        <li>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, doloremque? Molestias repudiandae totam deserunt
+          ipsam, a labore fuga provident tempora hic voluptate? Repellendus alias, minus modi rem nisi officiis nemo!
+        </li>
+        <li>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, doloremque? Molestias repudiandae totam deserunt
+          ipsam, a labore fuga provident tempora hic voluptate? Repellendus alias, minus modi rem nisi officiis nemo!
+        </li>
+        <li>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, doloremque? Molestias repudiandae totam deserunt
+          ipsam, a labore fuga provident tempora hic voluptate? Repellendus alias, minus modi rem nisi officiis nemo!
+        </li>
+        <li>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, doloremque? Molestias repudiandae totam deserunt
+          ipsam, a labore fuga provident tempora hic voluptate? Repellendus alias, minus modi rem nisi officiis nemo!
+        </li>
+        <li>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, doloremque? Molestias repudiandae totam deserunt
+          ipsam, a labore fuga provident tempora hic voluptate? Repellendus alias, minus modi rem nisi officiis nemo!
+        </li>
+      </List>
+      <button onClick={handleToTop}>up!</button>
     </Layout>
   );
 }
+
+const Box = styled.div`
+  position: fixed;
+  top: 0;
+  right: 0;
+  left: 0;
+  height: 32px;
+  /* z-index: 10; */
+  /* box-shadow: inset 0 0 10px blue; */
+  background-color: antiquewhite;
+`;
+
+const List = styled.ul`
+  margin: 0;
+  padding: 0;
+  li {
+    margin: 0;
+    padding: 0;
+    font-size: 30px;
+  }
+`;
