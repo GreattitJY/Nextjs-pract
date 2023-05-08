befd08702a164e451a93605930035bd603f70156
GreattitJY <great.tit.jaejae@gmail.com>
2023-05-08 22:25:52 +0900

    add: layout

---
+++ fresh/app/abc/def/page.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Def() {
+  return (
+    <div>
+      <h4>def페이지입니다.</h4>
+    </div>
+  );
+}
---
+++ fresh/app/abc/page.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Abc() {
+  return (
+    <div>
+      <h4>abc페이지에요</h4>
+    </div>
+  );
+}
