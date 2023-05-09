67581a5cb58f39bbd026752e9ef7a4c7fda31e85
GreattitJY <great.tit.jaejae@gmail.com>
2023-05-09 21:29:16 +0900

    add: cart

--- fresh/app/list/page.jsx
+++ fresh/app/list/page.jsx
@@ -1,15 +1,19 @@
 import React from "react";
+import Image from "next/image";
 
 export default function List() {
+  const products = ["Tomatoes", "Pastas", "Coconut"];
+
   return (
     <div>
       <h4 className="title">상품목록</h4>
-      <div className="food">
-        <h4>상품1 $40</h4>
-      </div>
-      <div className="food">
-        <h4>상품2 $40</h4>
-      </div>
+      {products.map((product, i) => {
+        return (
+          <div className="food" key={i}>
+            <h4>{product} $40</h4>
+          </div>
+        );
+      })}
     </div>
   );
 }
